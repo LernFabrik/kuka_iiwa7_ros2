862d62db2ea1d588922f144207d780aa9ad50653
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-10-24 08:50:34 +0200

    Install cyclone DDS in docker

--- docker/workspace-entrypoint.sh
+++ docker/workspace-entrypoint.sh
@@ -2,8 +2,10 @@
 # Build ROS dependency
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 echo "export ROS_DOMAIN_ID=69" >> ~/.bashrc
+echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 export ROS_DOMAIN_ID=69
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 sudo apt-get update
 rosdep update
